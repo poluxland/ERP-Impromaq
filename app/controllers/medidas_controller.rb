19797697785576eca10e672ad22bac26887c0946
Jose Ignacio Jerez <joseignaciojerez@gmail.com>
2020-06-22 16:26:55 -0400

    Skip authenticate in medidas

--- app/controllers/medidas_controller.rb
+++ app/controllers/medidas_controller.rb
@@ -1,4 +1,5 @@
 class MedidasController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:create, :show, :new]
   before_action :set_medida, only: [:show, :edit, :update, :destroy]
 
   # GET /medidas
