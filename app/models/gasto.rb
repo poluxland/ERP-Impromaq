369b5001815b9f1a7e408859c9e2ee2908cc1c1a
Jose Jerez <joseignaciojerez@gmail.com>
2020-07-21 15:25:45 -0400

    CSV enconding utf8

--- app/models/gasto.rb
+++ app/models/gasto.rb
@@ -5,7 +5,7 @@ require 'activerecord-import/active_record/adapters/postgresql_adapter'
 
 def self.my_import(file)
   gastos = []
-  CSV.foreach(file.path, headers: true, col_sep: ';') do |row|
+  CSV.foreach(file.path, headers: true, col_sep: ';', :encoding => 'ISO-8859-1') do |row|
     gastos << Gasto.new(row.to_h)
   end
   Gasto.import gastos, recursive: true
