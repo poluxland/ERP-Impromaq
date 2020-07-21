f7bc2d76158a75330ee2f5caeb5ab75a41f53bae
Jose Jerez <joseignaciojerez@gmail.com>
2020-07-21 11:36:45 -0400

    add emails of melon to medidas

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,8 +6,23 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.medidas.subject
   #
   def medidas
-    @greeting = "Hi"
+    @greeting = 'Hi'
 
-    mail to: "ignacio.sierra@msindustrial.cl, jose.jerez@msindustrial.cl, fernando.gonzalez@msindustrial.cl, supervisor.envasadora@msindustrial.cl"
+    mail to: "ignacio.sierra@msindustrial.cl,
+jose.jerez@msindustrial.cl,
+fernando.gonzalez@msindustrial.cl,
+supervisor.envasadora@msindustrial.cl,
+osvaldo.silva@meloncementos.cl,
+jefesturno.planta@melon.cl,
+abigail.bustos@meloncementos.cl,
+gustavo.villanueva@msindustrial.cl,
+renzo.allendes@meloncementos.cl,
+alejandro.olivares@msindustrial.cl,
+luis.estay@meloncementos.cl,
+sala.controlh9m21@melon.cl,
+ignacio.sierra@msindustrial.cl,
+claudio.villanueva@msindustrial.cl,
+marianelly.villarroel@meloncementos.cl,
+orlando.belmar@meloncementos.cl"
   end
 end
