032ded1311b8fb28e2b655e8dcf703b0817737a1
Jose Jerez <joseignaciojerez@gmail.com>
2021-02-19 11:03:42 -0300

    add claudio villanueva email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,6 @@ class UserMailer < ApplicationMailer
   def medidas
     @greeting = "Medidas " + Time.now.to_s
 
-    mail to: "ignacio.sierra@msindustrial.cl, jose.jerez@msindustrial.cl, fernando.gonzalez@msindustrial.cl, supervisor.envasadora@msindustrial.cl, osvaldo.silva@meloncementos.cl, jefesturno.planta@melon.cl, abigail.bustos@meloncementos.cl, gustavo.villanueva@msindustrial.cl, renzo.allendes@meloncementos.cl, alejandro.olivares@msindustrial.cl, luis.estay@meloncementos.cl, sala.controlh9m21@melon.cl, ignacio.sierra@msindustrial.cl, claudio.villanueva@msindustrial.cl, marianelly.villarroel@meloncementos.cl, orlando.belmar@meloncementos.cl, fiorella.cosmelli@meloncementos.cl, gerald.betancur@meloncementos.cl", subject: @greeting
+    mail to: "ignacio.sierra@msindustrial.cl, jose.jerez@msindustrial.cl, fernando.gonzalez@msindustrial.cl, supervisor.envasadora@msindustrial.cl, osvaldo.silva@meloncementos.cl, jefesturno.planta@melon.cl, abigail.bustos@meloncementos.cl, gustavo.villanueva@msindustrial.cl, renzo.allendes@meloncementos.cl, alejandro.olivares@msindustrial.cl, luis.estay@meloncementos.cl, sala.controlh9m21@melon.cl, ignacio.sierra@msindustrial.cl, claudio.villanueva@msindustrial.cl, marianelly.villarroel@meloncementos.cl, orlando.belmar@meloncementos.cl, fiorella.cosmelli@meloncementos.cl, gerald.betancur@meloncementos.cl", "claudio.villanueva@msindustrial.cl", subject: @greeting
   end
 end
