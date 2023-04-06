2a7612edceda097b0a0cd4b544f078fb209d7ae9
Jose Jerez <joseignaciojerez@gmail.com>
2023-04-06 18:15:18 -0400

    add mascos prospero to report

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ApplicationMailer
   def medidas
     @greeting = "Reporte Impromaq medidas de silos y stock"
 
-    mail to: "jorge.frez@msindustrial.cl, jose.jerez@msindustrial.cl, fernando.gonzalez@msindustrial.cl, supervisor.envasadora@msindustrial.cl, osvaldo.silva@meloncementos.cl, jefesturno.planta@melon.cl, abigail.bustos@meloncementos.cl, rodrigo.trigo@meloncementos.cl, renzo.allendes@meloncementos.cl, alejandro.olivares@msindustrial.cl, luis.estay@meloncementos.cl, sala.controlh9m21@melon.cl, carlos.gonzalezm@meloncementos.cl, claudio.villanueva@msindustrial.cl, marianelly.villarroel@meloncementos.cl, orlando.belmar@meloncementos.cl, fiorella.cosmelli@meloncementos.cl, gerald.betancur@meloncementos.cl, claudio.villanueva@msindustrial.cl, gaston.guerrero@meloncementos.cl, aldo.escobar@meloncementos.cl, guillermo.roco@meloncementos.cl, roberto.leiva@meloncementos.cl", subject: @greeting
+    mail to: "jorge.frez@msindustrial.cl, jose.jerez@msindustrial.cl, fernando.gonzalez@msindustrial.cl, supervisor.envasadora@msindustrial.cl, osvaldo.silva@meloncementos.cl, jefesturno.planta@melon.cl, abigail.bustos@meloncementos.cl, rodrigo.trigo@meloncementos.cl, renzo.allendes@meloncementos.cl, alejandro.olivares@msindustrial.cl, luis.estay@meloncementos.cl, sala.controlh9m21@melon.cl, carlos.gonzalezm@meloncementos.cl, claudio.villanueva@msindustrial.cl, marianelly.villarroel@meloncementos.cl, orlando.belmar@meloncementos.cl, fiorella.cosmelli@meloncementos.cl, marcos.prospero@msindustrial.cl, gaston.guerrero@meloncementos.cl, aldo.escobar@meloncementos.cl, guillermo.roco@meloncementos.cl, roberto.leiva@meloncementos.cl", subject: @greeting
   end
 
   def units
