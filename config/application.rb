fd19c970ea67a9cd877a9b1f7f16a7e5dee3b7c0
Jose Jerez <joseignaciojerez@gmail.com>
2023-04-13 16:17:01 -0400

    soplado form

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module RailsImpromaq
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'Santiago'
+
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
