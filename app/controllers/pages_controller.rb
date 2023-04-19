948c350923ceace45a5bedd6b4999980f662d27d
Jose Jerez <joseignaciojerez@gmail.com>
2023-04-19 11:46:50 -0400

    trucks resume

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
   def home
-      @trucks = Truck.all
+      @trucks = Truck.includes(:checklist, :equipo, :intervention).all
   end
 end
