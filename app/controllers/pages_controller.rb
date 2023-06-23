20c953d04ca1327a082c117857506e16438fde66
Jose Jerez <joseignaciojerez@gmail.com>
2023-06-23 10:45:21 -0400

    checklist mod in main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,7 @@ class PagesController < ApplicationController
 
   def home
       @trucks = Truck.includes(:checklist, :equipo, :intervention).all
+      @trucks_ptm = @trucks.where(modelo: 'PTM')
+      @trucks_lca = @trucks.where.not(modelo: 'PTM')
   end
 end
