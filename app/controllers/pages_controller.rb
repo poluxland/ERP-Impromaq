3d2b58c1ecc7882556c8e68fb5698f3519ceb32d
Jose Jerez <joseignaciojerez@gmail.com>
2025-03-20 12:52:12 -0300

    cjecklist table

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,9 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
-  def estadoequipos
-    @trucks = Truck.includes(:checklist, :equipo, :intervention).all
-    @trucks_ptm = @trucks.where(planta: 'PTM')
-    @trucks_lca = @trucks.where.not(planta: 'PTM')
+  def home
+    @trucks = Truck.includes(:checklists, :equipos, :interventions).all
+
   end
 
  def reporte
