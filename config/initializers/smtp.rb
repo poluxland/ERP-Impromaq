6cbd496f07a420351e3a673522c2717911ae4b92
Jose Jerez <joseignaciojerez@gmail.com>
2025-04-30 16:04:51 -0400

    gmail

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,11 +3,13 @@ ActionMailer::Base.perform_deliveries    = true
 ActionMailer::Base.raise_delivery_errors = true
 
 ActionMailer::Base.smtp_settings = {
-  address:              'smtp.office365.com',
+  address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'outlook.com',
-  user_name:            ENV['OUTLOOK_USER'],    # impromaq.ltda@outlook.com
-  password:             ENV['OUTLOOK_PASS'],    # la App Password de Microsoft
-  authentication:       :login,
-  enable_starttls_auto: true
+  domain:               'gmail.com',
+  user_name:            ENV['GMAIL_ADDRESS'],
+  password:             ENV['GMAIL_APP_PASSWORD'],
+  authentication:       :plain,
+  enable_starttls_auto: true,
+  open_timeout:         30,
+  read_timeout:         30
 }
