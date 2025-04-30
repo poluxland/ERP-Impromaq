e286832f5298cca1e40c6c1275285e061ca15bdc
Jose Jerez <joseignaciojerez@gmail.com>
2025-04-30 15:49:06 -0400

    smtp

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.perform_deliveries    = true
 ActionMailer::Base.raise_delivery_errors = true
 
 ActionMailer::Base.smtp_settings = {
-  address:              'smtp-mail.outlook.com',
+  address:              'smtp.office365.com',
   port:                 587,
   domain:               'outlook.com',
   user_name:            ENV['OUTLOOK_USER'],    # impromaq.ltda@outlook.com
