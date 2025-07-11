a3148c66f6ae03e72a3df13f100f950b529a6820
Jose Jerez <joseignaciojerez@gmail.com>
2025-07-10 23:16:38 -0400

    new stack

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
+
   def home
     @trucks = Truck.includes(:checklists, :equipos, :interventions)
                    .where.not(planta: 'X')
