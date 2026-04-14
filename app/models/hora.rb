04a9937e8aca7ec44c8cdfaaf54ef665077df4a4
Jose Ignacio Jerez Torrens <joseignaciojereztorrens@192.168.1.27>
2026-04-14 14:20:15 -0400

    time

--- app/models/hora.rb
+++ app/models/hora.rb
@@ -1,6 +1,7 @@
 class Hora < ApplicationRecord
+  self.skip_time_zone_conversion_for_attributes = [:inicio, :termino]
+
   def self.enviar_reporte_horas_pendientes
     UserMailer.horas_pendientes_revision.deliver_now
   end
 end
-
